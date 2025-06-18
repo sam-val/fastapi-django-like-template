c85d1c340b8f70d5ff93d5e77c11c3c5e46f278e
sam-val <ineptcode@gmail.com>
2025-06-18 18:56:12 +0700

    update main.py

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
 
+from backend.config.settings import get_settings
 from backend.config.urls import router as root_router
 
-app = FastAPI(
-    title="FastAPI Django-like Folder Structure",
-    version="0.0.1",
-)
 
-app.include_router(root_router)
+def create_app() -> FastAPI:
+    settings = get_settings()
+    is_prod = settings.MODE == "prod"
+
+    app = FastAPI(
+        title="FastAPI Django-like Folder Structure",
+        debug=settings.DEBUG,
+        version="0.0.1",
+        docs_url=None if is_prod else "/docs",
+        redoc_url=None if is_prod else "/redoc",
+        openapi_url=None if is_prod else "/openapi.json",
+    )
+
+    app.include_router(root_router)
+    return app
+
+
+app = create_app()
